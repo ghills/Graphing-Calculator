a8171cb305bf89bf19c37a0009d1939c27f32ef6
Gavin Hills <ghills@gmail.com>
2011-08-09 21:49:51 -0700

    Initial Commit

---
+++ GraphingCalculator/GraphingCalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GraphingCalculatorAppDelegate.h
+//  GraphingCalculator
+//
+//  Created by Gavin Hills on 8/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphingCalculatorAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
